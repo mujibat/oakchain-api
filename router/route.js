b99cfb471a1cab8aa31141d05db48a649d067744
f-deniyi <holarfemilekan049@gmail.com>
2023-08-21 12:35:14 -0700

    status check

--- router/route.js
+++ router/route.js
@@ -3,4 +3,8 @@ import { signinUser, updateUser } from '../controllers/auth.js'
 const router = Router()
 
 router.route('/auth').post(signinUser).put(updateUser)
+router.get('/status', (req, res) => {
+    return res.json('Oak chain-status')
+})
+
 export default router
