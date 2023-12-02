6d3a2c6424fbbadb493aeb5741063f1f48ea8b8b
Samson Ajulor <samsonajulor@gmail.com>
2023-12-02 11:07:10 +0100

    privatized the packages

--- backend/src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import env from './env';
-import logger from './logger';
-import cloudinary from './cloudinaryConfig';
-
-const SAMSON_CONFIGS = { env, logger, cloudinary };
-
-export default SAMSON_CONFIGS;
